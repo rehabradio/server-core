38a2d5efce1d3b4162f61be69eb9b969895be78c
Mark McConnell <mark@rehabstudio.com>
2014-08-06 14:07:41 +0100

    radio_playlists > Tests - skeleton created

--- radio_playlists/tests.py
+++ radio_playlists/tests.py
@@ -1,3 +1,46 @@
+import json
 from django.test import TestCase
 
-# Create your tests here.
+
+class BaseTestCase(TestCase):
+    fixtures = ['radio/fixtures/testdata.json']
+
+
+class PlaylistViewSetTestCase(BaseTestCase):
+    def test_list(self):
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_retrieve(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_partial_update(self):
+        pass
+
+    def test_destroy(self):
+        pass
+
+
+class PlaylistTrackViewSetTestCase(BaseTestCase):
+    def test_list(self):
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_retrieve(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_partial_update(self):
+        pass
+
+    def test_destroy(self):
+        pass
