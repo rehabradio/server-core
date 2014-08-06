041d5e455f82738fde56671d873783c1399a6f49
Mark McConnell <mark@rehabstudio.com>
2014-08-06 15:13:50 +0100

    radio_playlist > tests > PlaylistViewSetTestCase - get and post methods added

--- radio_playlists/tests.py
+++ radio_playlists/tests.py
@@ -1,5 +1,6 @@
 import json
 from django.test import TestCase
+from django.test.client import Client
 
 
 class BaseTestCase(TestCase):
@@ -7,11 +8,61 @@ class BaseTestCase(TestCase):
 
 
 class PlaylistViewSetTestCase(BaseTestCase):
+    def setUp(self):
+        self.client = Client()
+        login = self.client.login(username='admin', password='redc@t9912')
+        self.assertEqual(login, True)
+
+
     def test_list(self):
-        pass
+        result_attrs = (
+            'count',
+            'next',
+            'previous',
+            'results',
+        )
+
+        track_attrs = (
+            'id',
+            'name',
+            'description',
+        )
+
+        resp = self.client.get('/api/playlists/')
+        data = json.loads(resp.content)
+        playlists = data['results'][0]
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(set(result_attrs) <= set(data))
+        self.assertTrue(set(track_attrs) <= set(playlists))
 
     def test_create(self):
-        pass
+        post_data = {
+            'name': 'test playlist',
+            'description': 'a playlist for tdd',
+        }
+
+        resp = self.client.post('/api/playlists/', data=post_data)
+        data = json.loads(resp.content)
+
+        self.assertEqual(resp.status_code, 201)
+        self.assertRegexpMatches(str(data['id']), r'[0-9]+')
+        self.assertEqual(data['name'], post_data['name'])
+        self.assertEqual(data['description'], post_data['description'])
+
+
+    def test_create_with_empty_values(self):
+        post_data = {
+            'name': '',
+            'description': '',
+        }
+
+        resp = self.client.post('/api/playlists/', data=post_data)
+        data = json.loads(resp.content)
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(data['name'], ['This field is required.'])
+        self.assertEqual(data['description'], ['This field is required.'])
 
     def test_retrieve(self):
         pass
