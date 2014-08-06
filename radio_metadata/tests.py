c5db5d2d8fcf3b9b67d333362aa2ef027e6dd484
Mark McConnell <mark@rehabstudio.com>
2014-08-06 12:37:38 +0100

    radio_metadata > tests - Base/Lookup/Search functions tested

--- radio_metadata/tests.py
+++ radio_metadata/tests.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase
 
 
@@ -8,34 +9,103 @@ class BaseTestCase(TestCase):
 class MetadataAPIRootViewTestCase(BaseTestCase):
     def test_get(self):
         resp = self.client.get('/api/metadata/')
-        self.assertEqual(resp.status_code, 200)
+        data = json.loads(resp.content)
 
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(data['endpoints'])
+        self.assertTrue(data['endpoints']['search'])
+        self.assertTrue(data['endpoints']['lookup'])
 
 class LookupRootViewTestCase(BaseTestCase):
     def test_get(self):
         resp = self.client.get('/api/metadata/lookup/')
+        data = json.loads(resp.content)
+
         self.assertEqual(resp.status_code, 200)
+        self.assertTrue(data['endpoints'])
+        self.assertTrue(data['endpoints']['soundcloud'])
+        self.assertTrue(data['endpoints']['soundcloud']['tracks'])
+        self.assertTrue(data['endpoints']['spotify'])
+        self.assertTrue(data['endpoints']['spotify']['tracks'])
 
 
 class LookupViewTestCase(BaseTestCase):
     def test_get(self):
-        resp = self.client.get('/api/metadata/lookup/spotify/6MeNtkNT4ENE5yohNvGqd4/')
-        self.assertEqual(resp.status_code, 200)
+        track_attrs = (
+            'source_type',
+            'source_id',
+            'name',
+            'artists',
+            'album',
+            'duration_ms',
+            'preview_url',
+            'track_number',
+            'image_small',
+            'image_medium',
+            'image_large',
+        )
 
-        resp = self.client.get('/api/metadata/lookup/soundcloud/153868082/')
-        self.assertEqual(resp.status_code, 200)
+        resp1 = self.client.get('/api/metadata/lookup/spotify/6MeNtkNT4ENE5yohNvGqd4/')
+        data1 = json.loads(resp1.content)
+
+        self.assertEqual(resp1.status_code, 200)
+        self.assertTrue(set(track_attrs) <= set(data1))
+
+        resp2 = self.client.get('/api/metadata/lookup/soundcloud/153868082/')
+        data2 = json.loads(resp2.content)
+
+        self.assertEqual(resp2.status_code, 200)
+        self.assertTrue(set(track_attrs) <= set(data2))
+
 
 
 class SearchRootViewTestCase(BaseTestCase):
     def test_get(self):
         resp = self.client.get('/api/metadata/search/')
+        data = json.loads(resp.content)
         self.assertEqual(resp.status_code, 200)
+
+        self.assertTrue(data['endpoints'])
+        self.assertTrue(data['endpoints']['soundcloud'])
+        self.assertTrue(data['endpoints']['soundcloud']['tracks'])
+        self.assertTrue(data['endpoints']['spotify'])
+        self.assertTrue(data['endpoints']['spotify']['tracks'])
 
 
 class SearchViewTestCase(BaseTestCase):
     def test_get(self):
-        resp = self.client.get('/api/metadata/search/spotify/?q=Haim/')
-        self.assertEqual(resp.status_code, 200)
+        result_attrs = (
+            'count',
+            'next',
+            'previous',
+            'results',
+        )
+        track_attrs = (
+            'source_type',
+            'source_id',
+            'name',
+            'artists',
+            'album',
+            'duration_ms',
+            'preview_url',
+            'track_number',
+            'image_small',
+            'image_medium',
+            'image_large',
+        )
 
-        resp = self.client.get('/api/metadata/search/soundcloud/?q=narsti/')
-        self.assertEqual(resp.status_code, 200)
+        resp1 = self.client.get('/api/metadata/search/spotify/?q=Haim/')
+        data1 = json.loads(resp1.content)
+        tracks1 = data1['results'][0]
+
+        self.assertEqual(resp1.status_code, 200)
+        self.assertTrue(set(result_attrs) <= set(data1))
+        self.assertTrue(set(track_attrs) <= set(tracks1))
+
+        resp2 = self.client.get('/api/metadata/search/soundcloud/?q=narsti/')
+        data2 = json.loads(resp2.content)
+        tracks2 = data2['results'][0]
+
+        self.assertEqual(resp2.status_code, 200)
+        self.assertTrue(set(result_attrs) <= set(data2))
+        self.assertTrue(set(track_attrs) <= set(tracks2))
