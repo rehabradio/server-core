a4324cf3904dab1d2b7a4ccf581bffaa64c4d4af
Mark McConnell <mark@rehabstudio.com>
2014-08-08 14:21:09 +0100

    migration fix

--- radio_metadata/migrations/0004_auto_20140808_1103.py
+++ radio_metadata/migrations/0004_auto_20140808_1103.py
@@ -2,7 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.contrib.auth.models import User
 from django.conf import settings
+
+
+def update_owner(apps, schema_editor):
+    # We can't import the Person model directly as it may be a newer
+    # version than this migration expects. We use the historical version.
+    Track = apps.get_model("radio_metadata", "Track")
+    user = User.objects.get(id=1)
+    for track in Track.objects.all():
+        track.owner_id = user.id
+        track.save()
 
 
 class Migration(migrations.Migration):
@@ -12,6 +23,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(update_owner),
         migrations.AlterField(
             model_name='track',
             name='owner',
