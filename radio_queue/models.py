b16fe79235699e609af2802eb2c21d446e188d4f
mark <mark@rehabstudio.com>
2014-08-13 09:35:38 +0100

    player removed from queue model

--- radio_queue/models.py
+++ radio_queue/models.py
@@ -2,19 +2,14 @@ from django.db import models
 
 # local imports
 from radio_metadata.models import Track
-from radio_players.models import Player
 
 
 class Queue(models.Model):
     name = models.CharField(max_length=500)
     description = models.CharField(max_length=500)
-    player = models.ForeignKey(Player, null=True)
     owner = models.ForeignKey('auth.User')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        unique_together = (('id', 'player'),)
 
 
 class QueueTrack(models.Model):
