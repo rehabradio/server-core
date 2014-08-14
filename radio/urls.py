9938b38db8dbf46ca0e7ae0cfd3076d18cd9b2c8
mark <mark@rehabstudio.com>
2014-08-14 17:51:39 +0100

    wsgi updated to use Cling to serve static files

--- radio/urls.py
+++ radio/urls.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.conf.urls import *
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 
