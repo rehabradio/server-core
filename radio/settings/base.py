f6273109a07531efcaeca0d9e3f5b6e123e792dc
mark <mark@rehabstudio.com>
2014-08-14 17:10:08 +0100

    x-googeauth_token added to cors allowed headers

--- radio/settings/base.py
+++ radio/settings/base.py
@@ -66,6 +66,16 @@ MIDDLEWARE_CLASSES = (
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = (
+        'x-requested-with',
+        'content-type',
+        'accept',
+        'origin',
+        'authorization',
+        'x-csrftoken',
+        'x_google_auth_token'
+    )
+
 ROOT_URLCONF = 'radio.urls'
 
 WSGI_APPLICATION = 'radio.wsgi.application'
