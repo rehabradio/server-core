bf27fcb5d280e57d7cfb301a7a95d5ea9c92473b
mark <mark@rehabstudio.com>
2014-08-14 17:29:46 +0100

    debug toolbar deactivated

--- radio/settings/base.py
+++ radio/settings/base.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
