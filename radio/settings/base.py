2a546e2504e9f157cb53e9326c27d1944a1fc045
mark <mark@rehabstudio.com>
2014-08-19 17:16:27 +0100

    directory structure updated, all apps moved into "apps" folder

--- radio/settings/base.py
+++ radio/settings/base.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import re
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+import sys
+
+BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+sys.path.insert(0, os.path.abspath(os.path.join(BASE_DIR, os.pardir, 'apps')))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
