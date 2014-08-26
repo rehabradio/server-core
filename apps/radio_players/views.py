38e30816df46f601e286b543f1f222df624dc89b
mark <mark@rehabstudio.com>
2014-08-26 14:42:56 +0100

    radio_players > views - retrieve method now accepts record id, as well as token

--- apps/radio_players/views.py
+++ apps/radio_players/views.py
@@ -1,3 +1,4 @@
+# std-lib imports
 import uuid
 # third-party imports
 from django.contrib.auth.models import User
@@ -5,7 +6,6 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-
 # local imports
 from .models import Player
 from .serializers import PlayerSerializer
@@ -14,29 +14,34 @@ from .serializers import PlayerSerializer
 class PlayerViewSet(viewsets.ModelViewSet):
     """
     CRUD API endpoints that allow managing connected players.
-    User must be admin
+    User must be admin.
     """
     permissions = (IsAdminUser,)
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
     def retrieve(self, request, *args, **kwargs):
-        record = Player.objects.get(token=kwargs['pk'])
+        """Fetch record by token or id."""
+        try:
+            record = Player.objects.get(id=int(kwargs['pk']))
+        except:
+            record = Player.objects.get(token=kwargs['pk'])
+
         serializer = self.serializer_class(record)
         return Response(serializer.data)
 
     @action()
     def mopidy_event(self, request, *args, **kwargs):
+        """Tracks all event on the mopidy server."""
         return Response(request.DATA)
 
     @action()
     def mopidy_status(self, request, *args, **kwargs):
+        """Tracks all status updates on the mopidy server."""
         return Response(request.DATA)
 
     def post_save(self, player, created=False):
-        """
-        On creation, create a token
-        """
+        """On creation, create a user account and auth token."""
         if created:
             token = uuid.uuid4()
 
