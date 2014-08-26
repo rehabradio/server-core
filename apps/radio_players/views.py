70acdd319a3c9d7c8fe2cdb7b001160cd4ad1bce
mark <mark@rehabstudio.com>
2014-08-26 11:08:57 +0100

    radio_players > views - mopdiy event and status endpoints added

--- apps/radio_players/views.py
+++ apps/radio_players/views.py
@@ -1,7 +1,9 @@
 import uuid
 # third-party imports
 from rest_framework import viewsets
+from rest_framework.decorators import action, link
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 # local imports
 from .models import Player
@@ -16,6 +18,19 @@ class PlayerViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAdminUser,)
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        record = Player.objects.get(token=kwargs['pk'])
+        serializer = self.serializer_class(record)
+        return Response(serializer.data)
+
+    @action()
+    def mopidy_event(self, request, *args, **kwargs):
+        return Response(request.DATA)
+
+    @action()
+    def mopidy_status(self, request, *args, **kwargs):
+        return Response(request.DATA)
 
     def post_save(self, player, created=False):
         """
