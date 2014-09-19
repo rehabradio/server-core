b91544d427dd7d888e0c92465539ffb95f3f80c2
Mark McConnell <mark@rehabstudio.com>
2014-09-19 14:54:38 +0100

    radio > utils > caching - replaces whitespace with "20%"

--- app/radio/utils/cache.py
+++ app/radio/utils/cache.py
@@ -6,7 +6,9 @@ def build_key(*args):
     """Build a key for cached data, appends a timestamp onto each key."""
     cache_name = []
     for attr in args:
-        cache_name.append(str(attr))
+        string = str(attr)
+        clearn_string = string.replace(' ', '%20')
+        cache_name.append(clearn_string)
     cache_name.append(str(datetime.datetime.utcnow().strftime('%Y%m%d')))
 
     return '-'.join(cache_name)
