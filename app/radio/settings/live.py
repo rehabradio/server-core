6b72a758b4c34de9d4dc3a506163a9c58e793c38
Mark McConnell <mark@rehabstudio.com>
2015-01-22 12:29:14 +0000

    redis caching disabled

--- app/radio/settings/live.py
+++ app/radio/settings/live.py
@@ -12,13 +12,13 @@ DATABASES = {
 }
 
 # Django Caching settings
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': os.environ.get('REDIS_LOCATION', '127.0.0.1:6379:1'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
-            'SOCKET_TIMEOUT': 5,
-        },
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'redis_cache.cache.RedisCache',
+#         'LOCATION': os.environ.get('REDIS_LOCATION', '127.0.0.1:6379:1'),
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+#             'SOCKET_TIMEOUT': 5,
+#         },
+#     }
+# }
